b85b392bbac4b69f87dfa93dd02677ae3042b69d
Lucas Loureiro <lucas-loureiro@outlook.com>
2024-03-09 21:11:41 -0300

    test: add db url log for testing

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import dotenv from 'dotenv'
 import 'express-async-errors'
 
 import { app } from './app'
+import { getEnvironmentVariable } from './utils/getEnvironmentVariable'
 
 dotenv.config()
 
 const PORT = 4000
 
 const API_RUNNING = 'API em execução na porta'
+
+logger.debug({}, getEnvironmentVariable('DATABASE_URL'))
 
 app.listen(PORT, () => {
   logger.info(`${API_RUNNING} ${PORT}`)
