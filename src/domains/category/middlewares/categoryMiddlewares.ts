766631feb6a99c689ca7480ec3d39b269038d102
Lucas Loureiro <lucas-loureiro@outlook.com>
2024-12-12 21:09:16 -0300

    fix: remove find all categories query params validation

--- src/domains/category/middlewares/categoryMiddlewares.ts
+++ src/domains/category/middlewares/categoryMiddlewares.ts
@@ -1,10 +1,8 @@
 import { createOnePayloadValidation } from './createOnePayloadValidation'
-import { findManyQueryParamsValidation } from './findManyQueryParamsValidation'
 import { updateOnePayloadValidation } from './updateOnePayloadValidation'
 
 const categoryMiddlewares = {
   createOnePayloadValidation,
-  findManyQueryParamsValidation,
   updateOnePayloadValidation
 }
 
