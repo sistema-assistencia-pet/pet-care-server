903245314e647e8479fec2ff0576e07b57d60faa
Lucas Loureiro <lucas-loureiro@outlook.com>
2025-01-25 15:07:42 -0300

    feat: update default server port from 4000 to 7000

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { apiName } from './apiConfig'
 
 dotenv.config()
 
-const DEFAULT_SERVER_PORT = 4000
+const DEFAULT_SERVER_PORT = 7000
 // eslint-disable-next-line @typescript-eslint/dot-notation
 const SERVER_PORT = process.env['SERVER_PORT'] ?? DEFAULT_SERVER_PORT
 
