162ec2fb1755589ed041e4f340808106dc1c5485
Lucas Loureiro <lucas-loureiro@outlook.com>
2025-04-14 20:12:09 -0300

    fix: change from find user to find member

--- src/domains/voucher/middlewares/replaceResquestData.ts
+++ src/domains/voucher/middlewares/replaceResquestData.ts
@@ -1,16 +1,16 @@
 import { type NextFunction, type Request, type Response } from 'express'
 
 import { BadRequestError } from '../../../errors'
-import { userRepositories } from '../../user/repositories/userRepositories'
 import { clientRepositories } from '../../client/repositories/clientRepositories'
 import { MASTER_CLIENT_CNPJ } from '../../../apiConfig'
+import { memberRepositories } from '../../member/repositories/memberRepositories'
 
 export async function replaceResquestData (req: Request, _res: Response, next: NextFunction): Promise<void> {
   const cpf = req.body.cpf as string
 
-  const voucherUser = await userRepositories.findOne({ cpf })
-  if (voucherUser === null) throw new BadRequestError('Usuário não encontrado.')
-  req.headers['request-user-id'] = voucherUser.id
+  const voucherMember = await memberRepositories.findOne({ cpf }, false)
+  if (voucherMember === null) throw new BadRequestError('Associado não encontrado.')
+  req.headers['request-user-id'] = voucherMember.id
 
   const masterClient = await clientRepositories.findOne({ cnpj: MASTER_CLIENT_CNPJ }, false)
   if (masterClient === null) throw new BadRequestError('Cliente não encontrado.')
